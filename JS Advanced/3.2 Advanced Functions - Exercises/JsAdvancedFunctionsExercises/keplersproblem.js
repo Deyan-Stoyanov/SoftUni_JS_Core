4343d4468cc7edd4aae04c278635e54203a04a36
Deyan Stoyanov <32268625+Deyan-Stoyanov@users.noreply.github.com>
2018-10-26 18:25:01 +0300

    Delete keplersproblem.js

--- JS Advanced/3.2 Advanced Functions - Exercises/JsAdvancedFunctionsExercises/keplersproblem.js
+++
@@ -1,7 +0,0 @@
-function solveEquation(meanAnomaly, eccentricity){
-    let eccentricalAnomaly = 0;
-
-    return Math.ceil(eccentricalAnomaly * 1000000000) / 1000000000;
-}
-
-console.log(solveEquation(3.1415926535, 0.75));
