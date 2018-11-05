6757629571464dd61379ad5b4d79ea71b2571e82
Deyan-Stoyanov <deyan959@gmail.com>
2018-11-05 13:19:17 +0200

    Added JS Classes Lab Solutions

---
+++ JS Advanced/6.1 JS Classes/JsClassesLab/circle.js
@@ -0,0 +1,14 @@
+class Circle{
+    constructor(radius){
+        this.radius = radius;
+    }
+    set diameter(diameter){
+        this.radius = diameter / 2;
+    }
+    get diameter(){
+        return this.radius * 2;
+    }
+    get area(){
+        return Math.PI * this.radius * this.radius;
+    }
+}
---
+++ JS Advanced/6.1 JS Classes/JsClassesLab/rectangle.js
@@ -0,0 +1,8 @@
+class Rectangle {
+    constructor(width, height, color) {
+        [this.width, this.height, this.color] = [width, height, color];
+    }
+    calcArea() {
+        return this.width * this.height;
+    }
+}
